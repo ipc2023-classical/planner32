f7ee8044bc27ef35c432eefde106bb609326706e
Alvaro Torralba <torralba@cs.uni-saarland.de>
2020-06-23 20:53:48 +0200

    Updated scripts for the cluster

--- experiments/journal_dominance/configs/journal_1_analysis_dominance_relations.py
+++ experiments/journal_dominance/configs/journal_1_analysis_dominance_relations.py
@@ -26,31 +26,29 @@ CONFIGS["journal1-atomic"].append(configs.Config('blind', 'blind', "astar(blind(
 for sim in ["sim", "bisim", "ldsimalt", "noopsim", "qpos-10", "qtrade-10", "qrel-10", "qual-10"]:
     add_config("journal1-atomic", ["blind", sim, "atomic", "nosh", "gen"])
 
-add_config("journal1-atomic", ["blind", "qrel", "10", "atomic", "nosh", "gensucc"])
-add_config("journal1-atomic", ["blind", "qrel", "10", "atomic", "nosh", "succ"])
+add_config("journal1-atomic", ["blind", "qrel-10", "atomic", "nosh", "gensucc"])
+add_config("journal1-atomic", ["blind", "qrel-10", "atomic", "nosh", "succ"])
 
-add_config("journal1-atomic", ["blind", "qrel", "10", "atomic", "nosh", "gensucc", "simpletau"])
-add_config("journal1-atomic", ["blind", "qrel", "10", "atomic", "nosh", "gensucc", "recurtau"])
+add_config("journal1-atomic", ["blind", "qrel-10", "atomic", "nosh", "gensucc", "simpletau"])
+add_config("journal1-atomic", ["blind", "qrel-10", "atomic", "nosh", "gensucc", "recurtau"])
 
-add_config("journal1-atomic", ["blind", "qrel", "10", "dfp50k", "bissh", "gensucc", "simpletau"])
-add_config("journal1-atomic", ["blind", "qrel", "10", "dfp50k", "bissh", "gensucc", "recurtau"])
+add_config("journal1-atomic", ["blind", "qrel-10", "dfp50k", "bissh", "gensucc", "simpletau"])
+add_config("journal1-atomic", ["blind", "qrel-10", "dfp50k", "bissh", "gensucc", "recurtau"])
 
 for trval in [0, 1, 100, 1000]:
-    add_config("journal1-atomic", ["blind", "qrel", trval, "atomic", "nosh", pruning_type])
+    add_config("journal1-atomic", ["blind", "qrel", trval, "atomic", "nosh", "gen"])
 
 for sh in ["nosh", "bissh"]:
     for sim in ["sim", "bisim", "ldsimalt", "noopsim", "qpos-10", "qtrade-10", "qrel-10", "qual-10"]:
         add_config("journal1-nonatomic", ["blind", sim, "dfp50k", sh, "gen"])
 
-        add_config("journal1-nonatomic", ["blind", "qrel", "10", "dfp50k", sh, "gensucc"])
-        add_config("journal1-nonatomic", ["blind", "qrel", "10", "dfp50k", sh, "succ"])
-
-
-
+        add_config("journal1-nonatomic", ["blind", "qrel-10", "dfp50k", sh, "gensucc"])
+        add_config("journal1-nonatomic", ["blind", "qrel-10", "dfp50k", sh, "succ"])
 
 for sim in ["bisim", "ldsimalt", "qrel-10"]:
     add_config("journal2-heuristic", ["lmcut", sim, "atomic", "bissh", "gen"])
     add_config("journal2-heuristic", ["lmcut", sim, "dfp50k", "bissh", "gen"])
-    add_config("journal2-heuristic", ["lmcut", sim, "atomic", "bissh", "gensucc"])
-    add_config("journal2-heuristic", ["lmcut", sim, "dfp50k", "bissh", "gensucc"])
+
+add_config("journal2-heuristic", ["lmcut", "qrel-10", "atomic", "bissh", "gensucc"])
+add_config("journal2-heuristic", ["lmcut", "qrel-10", "dfp50k", "bissh", "gensucc"])
  
